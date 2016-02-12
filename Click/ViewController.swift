fd1bffce7017e8f6bdd4c75bfe41af20a2f847c0
hbc <bcxxxxxx@gmail.com>
2016-02-12 10:31:26 +0800

    feat: init project.

--- Click/ViewController.swift
+++ Click/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var button: UIButton!
+    var counter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        button = UIButton(type: UIButtonType.System) as UIButton
+        button.frame = CGRectMake(100, 100, 100, 50)
+        button.backgroundColor = UIColor.blueColor()
+        button.setTitle("\(counter)", forState: UIControlState.Normal)
+        button.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        button.addTarget(self, action: "onButtonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(button)
+        
+        self.view.backgroundColor = UIColor.whiteColor()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func onButtonClicked(sender: UIButton!) {
+        counter = counter + 1
+        button.setTitle("\(counter)", forState: UIControlState.Normal)
+        
+        if counter % 5 == 0 {
+            self.view.backgroundColor = UIColor(
+                red: CGFloat(Float(arc4random()) / Float(UINT32_MAX)),
+                green: CGFloat(Float(arc4random()) / Float(UINT32_MAX)),
+                blue: CGFloat(Float(arc4random()) / Float(UINT32_MAX)),
+                alpha: CGFloat(1.0))
+        }
     }
-
 
 }
 
